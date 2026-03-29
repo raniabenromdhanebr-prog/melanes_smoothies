b353175449810a53e7349b7869637a9c601db567
raniabenromdhanebr-prog <raniabenromdhane.br@gmail.com>
2026-03-29 14:44:40 +0200

    Update streamlit_app.py
    api2

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,6 @@ if list :
          st.success('Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json(),use_container_width=true)
+#st.text(smoothiefroot_response.json(),use_container_width=true)
+sf_df= st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
+
